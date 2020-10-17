31a9a9e0bed1f30134e560337e864fd4aea1b5a9
Ivan <komar.ivan.97@gmail.com>
2020-10-17 18:25:43 +0300

    Added header and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,14 +3,48 @@ import { SocialIcon } from 'react-social-icons';
 import './footer.css';
 const Footer = () => {
   return (
-      <footer className="App-footer">
-        <div className="inner-footer">
-        <SocialIcon url="http://twitter.com" />
-        <SocialIcon url="http://facebook.com" />
-        <SocialIcon url="http://vk.com" />
-        <SocialIcon url="http://google.com" />
-        </div>
-      </footer>
+    <footer>
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-4 text-center text-md-left ">
+                
+                <div class="py-0">
+                    <h3 class="my-4 text-white">About<span class="mx-2 font-italic text-warning ">DrukBox</span></h3>
+                    <p class="text-light py-4 mb-4">Copyright © 2020 All Rights Reserved by DrukBox.</p>
+                </div>
+            </div>
+            
+            <div class="col-md-4 text-white text-center text-md-left ">
+                <div class="py-2 my-4">
+                    <div>
+                        <p class="text-white"> <i class="fa fa-map-marker mx-2 "></i>
+                                Львівська політехніка,
+                                корпус 4
+                              </p>
+                    </div>
+
+                    <div> 
+                        <p><i class="fa fa-phone  mx-2 "></i> +99 99-9999999</p>
+                    </div>
+                    <div>
+                        <p><i class="fa fa-envelope  mx-2"></i><a href="mailto:example@gmail.com">example@gmail.com</a></p>
+                    </div>  
+                </div>  
+            </div>
+            
+            <div class="col-md-4 text-white my-4 text-center text-md-left ">
+                <span class=" font-weight-bold ">About us</span>
+      <p class="text-warning my-2" >Text</p>
+                <div class="py-2">
+                    <a href="#"><i class="fab fa-facebook fa-2x text-primary mx-3"></i></a>
+                    <a href="#"><i class="fab fa-google-plus fa-2x text-danger mx-3"></i></a>
+                    <a href="#"><i class="fab fa-twitter fa-2x text-info mx-3"></i></a>
+                    <a href="#"><i class="fab fa-youtube fa-2x text-danger mx-3"></i></a>
+                </div>
+            </div>
+        </div>  
+    </div>
+ </footer>
   );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,15 @@ const Header = () => {
   return (
       <header className="App-header">
         <div className="inner-header">
-        <h1>Hi!</h1>
+          <div className="header_left_links">
+            <a href="index.html" className="active">DrukBox</a>
+            <a href="printfile.html">Print File</a>
+            <a href="history.html">History</a>
+            <a href="addpages.html">Add pages</a>
+          </div>
+          <div className="header_right_links">
+            <a href="/">Sign in</a>
+          </div>
         </div>
       </header>
   );
