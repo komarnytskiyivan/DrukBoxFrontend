cdd1f2fca19b60801ceda9a876013a16448a8c3a
Ivan <komar.ivan.97@gmail.com>
2020-10-17 13:28:53 +0300

    Second commit

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+import Header from '../header';
+import Footer from '../footer';
+
+const App = () => {
+  return (
+    <div className="App">
+    <Header />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
